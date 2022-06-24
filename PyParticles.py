f4d3bf919ac3d5ddb845f428cc7e1e6da84d120c
Jermamite985 <106979386+MauzP@users.noreply.github.com>
2022-06-24 08:35:15 +0000

    added rectangles

--- PyParticles.py
+++ PyParticles.py
@@ -1,6 +1,6 @@
 import random, math, pygame
 
-width, height = 775, 420
+width, height = 775, 700
 pygame.init()
 pygame.display.set_caption('  Physikas')
 myfont = pygame.freetype.SysFont('Comic Sans MS', 20)
@@ -8,7 +8,8 @@ screen = pygame.display.set_mode((width, height))
 background_colour = 130, 209, 209
 gravity = (math.pi, 0.02)
 drag = 0.999
-elasticity = 0.75
+wallasticity = 0.85
+elasticity = 1
 mass_of_air = 0.2
 balls = ["ball1.png", "ball2.png" ,"ball3.png"]
 
@@ -28,6 +29,25 @@ def findParticle(particles, x, y):
 		if math.hypot(p.x-x, p.y-y) <= p.size:
 			return p
 
+#checks for collision
+def collide(p1, p2):
+	dx = p1.x - p2.x
+	dy = p1.y - p2.y
+	distance = math.hypot(dx, dy)
+	if distance < (p1.size)/2 + (p2.size)/2:
+		tangent = math.atan2(dy, dx)
+		angle = 0.5 * math.pi + tangent
+		angle1 = 2*tangent - p1.angle
+		angle2 = 2*tangent - p2.angle
+		speed1 = p2.speed*elasticity
+		speed2 = p1.speed*elasticity
+		(p1.angle, p1.speed) = (angle1, speed1)
+		(p2.angle, p2.speed) = (angle2, speed2)
+		p1.x += math.sin(angle)
+		p1.y -= math.cos(angle)
+		p2.x -= math.sin(angle)
+		p2.y += math.cos(angle)
+
 #class for a physics object
 class Particle:
 	def __init__(self, position, size, mass=1):
@@ -39,7 +59,7 @@ class Particle:
 		self.speed = 0.1
 		self.angle = 0
 		self.drag = (self.mass/(self.mass + mass_of_air)) ** self.size
-		self.image = balls[random.randint(0,len(balls)-1)]
+		self.image = balls[2]
 	def display(self):
 		ball = pygame.image.load(self.image).convert_alpha()
 		ball = pygame.transform.scale(ball, (self.size, self.size))
@@ -53,16 +73,26 @@ class Particle:
 		if self.x > width - self.size:
 			self.x = 2 * (width - self.size) - self.x
 			self.angle = - self.angle
-			self.speed *= elasticity
+			self.speed *= wallasticity
 		elif self.x < 0:
 			self.x = 0.2 * self.size - self.x
 			self.angle = - self.angle
-			self.speed *= elasticity
+			self.speed *= wallasticity
 		if self.y > height - self.size:
 			self.y = 2 * (height - self.size) - self.y
 			self.angle = math.pi - self.angle
-			self.speed *= elasticity
+			self.speed *= wallasticity
 		elif self.y < 0:
 			self.y = 0.5 * self.size
 			self.angle = math.pi - self.angle
-			self.speed *= elasticity
+			self.speed *= wallasticity
+
+class Obstacle:
+	def __init__(self, position, size):
+		self.x, self.y = position
+		self.width, self.height = size
+		self.colour = (255, 0, 0)
+		self.thickness = 1000
+	def display(self):
+		self.rect = pygame.rect.Rect(self.x,self.y,self.width,self.height)
+		pygame.draw.rect(screen, self.colour, self.rect)
--- main.py
+++ main.py
@@ -1,60 +1,60 @@
 import pygame, random, math, time, pygame.freetype
 from PyParticles import * 
-
-#variables
-#width, height = 500, 400
-width, height = 775, 420
 pygame.init()
-pygame.display.set_caption('  Physikas')
-myfont = pygame.freetype.SysFont('Comic Sans MS', 20)
-screen = pygame.display.set_mode((width, height))
-background_colour = 130, 209, 209
-gravity = (math.pi, 0.02)
-drag = 0.999
-elasticity = 0.75
-mass_of_air = 0.2
 
 #creates the specified number of particles
-number_of_particles = 50
+number_of_particles = 1
 my_particles = []
 for n in range(number_of_particles):
-	size = random.randint(20, 50)
-	density = random.randint(1, 20)
-	x = random.randint(size, width-size)
-	y = random.randint(size, height-size)
+	size = 50
+	density = random.randint(1, 5)
+	x = width / 2
+	y = 20
 	particle = Particle((x, y), size, density * size ** 2)
-	particle.speed = (random.randint(1, 10) / 5)
+	particle.speed = 0
 	particle.angle = random.uniform(0, math.pi*2)
 	my_particles.append(particle)
+
+number_of_obstacles = 1
+my_obstacles = []
+for n in range(number_of_obstacles):
+	size = (random.randint(20,50),random.randint(20,50))
+	position = (random.randint(0,width),random.randint(0,height))
+	obstacle = Obstacle(position, size)
+	my_obstacles.append(obstacle)
 	
 #runs the simulation
 running = True
 selected_particle = None
 while running:
-	screen.fill(background_colour)
-	myfont.render_to(screen, (10, 10), "Gravity is " + str(gravity[1]), (0, 0, 0))
-	myfont.render_to(screen, (10, 40), "Drag is " + str(drag), (0, 0, 0))
-	myfont.render_to(screen, (10, 70), "Elasticity is " + str(elasticity), (0, 0, 0))
-	myfont.render_to(screen, (10, 100), "Density of Medium is " + str(mass_of_air), (0, 0, 0))
-	for particle in my_particles:
-		if selected_particle:
-			(mouseX, mouseY) = pygame.mouse.get_pos()
-			dx = mouseX - selected_particle.x
-			dy = mouseY - selected_particle.y
-			selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
-			selected_particle.speed = math.hypot(dx, dy) * 0.1
-		if particle != selected_particle:
-			particle.move()
-			particle.bounce()
-		particle.display()
-	pygame.display.flip()
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.QUIT: 
 			running = False
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			(mouseX, mouseY) = pygame.mouse.get_pos()
 			selected_particle = findParticle(my_particles, mouseX, mouseY)
 		elif event.type == pygame.MOUSEBUTTONUP:		
 			selected_particle = None
+			
+	if selected_particle:
+		(mouseX, mouseY) = pygame.mouse.get_pos()
+		dx = mouseX - selected_particle.x
+		dy = mouseY - selected_particle.y
+		selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
+		selected_particle.speed = math.hypot(dx, dy) * 0.1
+		
+	screen.fill(background_colour)
+	
+	for i, particle in enumerate(my_particles):
+		particle.move()
+		particle.bounce()
+		for particle2 in my_particles[i+1:]:
+			collide(particle, particle2)
+		particle.display()
+
+	for i, obstacle in enumerate(my_obstacles):
+		obstacle.display()
+	
+	pygame.display.flip()
 
 pygame.quit ()
