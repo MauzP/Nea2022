05e4a94de38d3535ee660fcba5cd9a562e0619c8
Jermamite985 <106979386+MauzP@users.noreply.github.com>
2022-09-23 10:40:59 +0000

    removed physics sim mode

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import pygame, random, math, time, pygame.freetype
 from particleFuncs import * 
 pygame.init()
 
-while gors != "G" and gors != "S":
-	gors = input("Input G for game or S for paricle simulation ")
-
 #initializes the specified number of particles
-if gors == "G":
-	number_of_particles = 1
-else:
-	number_of_particles = int(input("How many particles? (May be laggy above 10.) "))
+number_of_particles = 1
 my_particles = []
 for n in range(number_of_particles):
 	size = 40
@@ -54,119 +48,88 @@ selected_particle = None
 activated = False
 timegone = False
 hasleadered = False
-if gors == "G":
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				#determines when to drop the ball
-				if event.key == pygame.K_SPACE:
-					activated = True
-					for i, particle in enumerate(my_particles):
-						#drops the ball at an angle
-						if particle.horizontal > 0:
-							particle.angle, particle.speed = addVectors((particle.angle, particle.speed), (math.pi/2, particle.horizontal))
-						else:
-							particle.angle, particle.speed = addVectors((particle.angle, particle.speed), (math.pi/2, particle.horizontal))
-			if event.type == pygame.QUIT: 
-				running = False
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				(mouseX, mouseY) = pygame.mouse.get_pos()
-				selected_particle = findParticle(my_particles, mouseX, mouseY)
-			elif event.type == pygame.MOUSEBUTTONUP:		
-				selected_particle = None
-	
-		if (pygame.time.get_ticks()/1000) >= 60:
-			timegone = True
-		
-		if not timegone:	
-			screen.blit(irithyll, (0, 0))
-			myfont.render_to(screen, (40, 0), "YOU HAVE 60 SECONDS", (0, 0, 0))
-			myfont.render_to(screen, (40, 60), "time "+str(math.floor((pygame.time.get_ticks()/1000))), (0, 0, 0))
-			myfont.render_to(screen, (40, 80), "score "+str(score), (0, 0, 0))
-			
-			for i, particle in enumerate(my_particles):
-				if activated:
-					particle.move()
-					particle.bounce()
-					for particle2 in my_particles[i+1:]:
-						collide(particle, particle2)
-					for obj in my_obstacles:
-						objcollide(particle, obj)
-					if particle.y > 725:
-						for i in range(0, len(goaln[0])):
-							if particle.x + 20 > goaln[0][i] and particle.x + 20 < goaln[0][i+1]:
-								score = score + goaln[2][i]
-						particle.x = width / 2
-						particle.y = 20
-						particle.speed = 0
-						activated = False
-				else:
-					particle.horizontalm()
-				particle.display()
-			
-			for i, obstacle in enumerate(my_obstacles):
-				obstacle.display()
-		
-			for i, goal in enumerate(my_goals):
-				goal.display()
-	
-		if timegone:
-			screen.fill((0,0,0))
-			gameover()
-			myfont2.render_to(screen, (225, 400), "SCORE "+str(score), (97, 33, 33))
-	
-			if hasleadered == False:
-				leader = open("leaderboard.txt", "a")
-				leader.write(str(score) + "\n")
-				leader.close()
-	
-				leader = open("leaderboard.txt", "r")
-				scores = []
-				for line in leader:
-					scores.append(int(line))
-				leader.close()
-	
-				quicksort(scores, 0, (len(scores)-1))
-				scores.reverse()
-	
-				hasleadered = True
-	
-			myfont2.render_to(screen, (225, 500), "Top 10 Scores:", (97, 33, 33))
-			for i in range(5):
-				myfont3.render_to(screen, (225, (580 + (40*i))), str(i + 1) + ") " + str(scores[i]), (97, 33, 33))
-				myfont3.render_to(screen, (500, (580 + (40*i))), str(i + 6) + ") " + str(scores[i + 5]), (97, 33, 33))
-			
-		pygame.time.Clock().tick()
-		pygame.display.flip()
-else:
-	while running:
-		activated = True
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: 
-				running = False
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				(mouseX, mouseY) = pygame.mouse.get_pos()
-				selected_particle = findParticle(my_particles, mouseX, mouseY)
-			elif event.type == pygame.MOUSEBUTTONUP:		
-				selected_particle = None
-
-		screen.blit(irithyll, (0, 0))
-			
-		if selected_particle:
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			#determines when to drop the ball
+			if event.key == pygame.K_SPACE:
+				activated = True
+				for i, particle in enumerate(my_particles):
+					#drops the ball at an angle
+					if particle.horizontal > 0:
+						particle.angle, particle.speed = addVectors((particle.angle, particle.speed), (math.pi/2, particle.horizontal))
+					else:
+						particle.angle, particle.speed = addVectors((particle.angle, particle.speed), (math.pi/2, particle.horizontal))
+		if event.type == pygame.QUIT: 
+			running = False
+		if event.type == pygame.MOUSEBUTTONDOWN:
 			(mouseX, mouseY) = pygame.mouse.get_pos()
-			dx = mouseX - selected_particle.x
-			dy = mouseY - selected_particle.y
-			selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
-			selected_particle.speed = math.hypot(dx, dy) * 0.1
+			selected_particle = findParticle(my_particles, mouseX, mouseY)
+		elif event.type == pygame.MOUSEBUTTONUP:		
+			selected_particle = None
 
+	if (pygame.time.get_ticks()/1000) >= 60:
+		timegone = True
+	
+	if not timegone:	
+		screen.blit(irithyll, (0, 0))
+		myfont.render_to(screen, (40, 0), "YOU HAVE 60 SECONDS", (0, 0, 0))
+		myfont.render_to(screen, (40, 60), "time "+str(math.floor((pygame.time.get_ticks()/1000))), (0, 0, 0))
+		myfont.render_to(screen, (40, 80), "score "+str(score), (0, 0, 0))
+		
 		for i, particle in enumerate(my_particles):
-			particle.move()
-			particle.bounce()
-			for particle2 in my_particles[i+1:]:
-				collide(particle, particle2)
+			if activated:
+				particle.move()
+				particle.bounce()
+				for particle2 in my_particles[i+1:]:
+					collide(particle, particle2)
+				for obj in my_obstacles:
+					objcollide(particle, obj)
+				if particle.y > 725:
+					for i in range(0, len(goaln[0])):
+						if particle.x + 20 > goaln[0][i] and particle.x + 20 < goaln[0][i+1]:
+							score = score + goaln[2][i]
+					particle.x = width / 2
+					particle.y = 20
+					particle.speed = 0
+					activated = False
+			else:
+				particle.horizontalm()
 			particle.display()
+		
+		for i, obstacle in enumerate(my_obstacles):
+			obstacle.display()
+	
+		for i, goal in enumerate(my_goals):
+			goal.display()
 
-		pygame.time.Clock().tick()
-		pygame.display.flip()
+	if timegone:
+		screen.fill((0,0,0))
+		gameover()
+		myfont2.render_to(screen, (225, 400), "SCORE "+str(score), (97, 33, 33))
+
+		if hasleadered == False:
+			leader = open("leaderboard.txt", "a")
+			leader.write(str(score) + "\n")
+			leader.close()
+
+			leader = open("leaderboard.txt", "r")
+			scores = []
+			for line in leader:
+				scores.append(int(line))
+			leader.close()
+
+			quicksort(scores, 0, (len(scores)-1))
+			scores.reverse()
+
+			hasleadered = True
+
+		myfont2.render_to(screen, (225, 500), "Top 10 Scores:", (97, 33, 33))
+		for i in range(5):
+			myfont3.render_to(screen, (225, (580 + (40*i))), str(i + 1) + ") " + str(scores[i]), (97, 33, 33))
+			myfont3.render_to(screen, (500, (580 + (40*i))), str(i + 6) + ") " + str(scores[i + 5]), (97, 33, 33))
+		
+	pygame.time.Clock().tick()
+	pygame.display.flip()
 
 pygame.quit ()
